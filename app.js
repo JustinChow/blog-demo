06a3155a954ec4358f9aec87d3999336ea79357a
Justin Chow <justin.chow@queensu.ca>
2020-07-28 19:50:46 -0400

    Fixing paths to frontend build files

--- app.js
+++ app.js
@@ -30,10 +30,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
 // when going to `/admin`, serve the admin frontend files as static files
-app.use('/admin', express.static(path.join(__dirname, '../blog-frontend-admin/build')));
+app.use('/admin', express.static(path.join(__dirname, '/blog-frontend-admin/build')));
 
 // Serve public frontend
-app.use(express.static(path.join(__dirname, '../blog-frontend-public/build')));
+app.use(express.static(path.join(__dirname, '/blog-frontend-public/build')));
 
 // Serve API routes
 app.use('/api', apiRouter);
@@ -41,12 +41,12 @@ app.use('/api', apiRouter);
 // Serve admin frontend
 app.get('/admin/*', (req, res) => {
     console.log("adminadminadminadmi");
-    res.sendFile(path.join(__dirname + '/../blog-frontend-admin/build/index.html'));
+    res.sendFile(path.join(__dirname + '/blog-frontend-admin/build/index.html'));
 });
 
 // "catchall" frontend handler to direct to public index.html file
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/../blog-frontend-public/build/index.html'));
+    res.sendFile(path.join(__dirname + '/blog-frontend-public/build/index.html'));
 });
 
 // catch 404 and forward to error handler
